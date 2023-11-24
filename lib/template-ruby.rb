0e370cf7e361b48b1dbccd9ee3d33290cced921b
Dorian Marié <dorian@thoughtbot.com>
2023-11-24 16:48:41 +0100

    add bin/template

--- lib/template-ruby.rb
+++ lib/template-ruby.rb
@@ -2,7 +2,7 @@ require "stringio"
 require "timeout"
 require "zeitwerk"
 require "code-ruby"
-require "template-ruby"
+require "language-ruby"
 
 loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.ignore("#{__dir__}/template-ruby.rb")
